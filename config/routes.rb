b2406c6abc9c057665f5eea2d3079696c29094c6
mollykwolf <mollykwolf@gmail.com>
2023-01-19 11:19:09 -0800

    second story finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'blogs' => 'blog#index'
+  get 'blogs' => 'blog#index', as: 'blogs'
+  get 'blogs/:id' => 'blog#show', as: 'blog'
   # root "articles#index"
 end
